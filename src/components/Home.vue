d51bc51485143a7c8974a3cf546c5ab502cadfa7
lifan <75855385@qq.com>
2019-02-16 17:07:49 +0800

    user功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,31 +14,32 @@
           background-color="#333744"
           text-color="#fff"
           active-text-color="#409EFF"
-          :style="menushow?'width:65px;':'width:200px;'"
           :collapse="menushow"
           :collapse-transition="false"
           :unique-opened="true"
+          :router="true"
         >
-          <el-submenu v-for="(item,i) in menuList" :key="item.id" :index="item.id + ''">
+          <el-submenu
+            v-for="(item,i) in menuList"
+            :key="item.id"
+            :index="item.id + ''"
+            :style="menushow?'width:65px;':'width:200px;'"
+          >
             <template slot="title">
               <i :class="'iconfont icon-' + menuIcon[i]"></i>
               <span>{{item.authName}}</span>
             </template>
             <el-menu-item-group>
-              <el-menu-item
-                v-for="item2 in item.children"
-                :key="item2.id"
-                :index="item2.id+ '-' +item2.id"
-              >
-              <i class="el-icon-menu"></i>
-              {{item.authName}}
+              <el-menu-item v-for="item2 in item.children" :key="item2.id" :index="item2.path">
+                <i class="el-icon-menu"></i>
+                {{item.authName}}
               </el-menu-item>
             </el-menu-item-group>
           </el-submenu>
         </el-menu>
       </el-aside>
       <el-main>
-        <router-view />
+        <router-view/>
       </el-main>
     </el-container>
   </el-container>
@@ -71,9 +72,11 @@ export default {
     },
     async getMenuList() {
       const { data: res } = await this.$http.get('menus')
-      if (res.meta.status === 200) {
-        this.menuList = res.data
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+        // console.log(res.data)
       }
+      this.menuList = res.data
     }
   }
 }
@@ -102,7 +105,6 @@ export default {
     }
   }
   .el-aside {
-    overflow: hidden;
     user-select: none;
     background: #333744;
     .switch {
---
+++ src/components/User.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h2>用户管理界面</h2>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card"></el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+    this.getUserList()
+  },
+  data() {
+    return {
+      userList: []
+    }
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$http.get('users', {
+        params: {
+          pagenum: 1,
+          pagesize: 5
+        }
+      })
+      if (res.meta.status === 200) {
+        this.userList = res.data.users
+        console.log(res)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
