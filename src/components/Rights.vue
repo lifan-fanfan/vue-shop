99a3a5fb58a0c6ec966a52a6d398b7ba404a9a39
lifan <75855385@qq.com>
2019-02-18 22:00:59 +0800

    创建rights

---
+++ src/components/Rights.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card class="box-card">
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column prop="date" label="日期" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  created() {
+
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
