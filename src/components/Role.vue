56846322cd8ef36aed6ada0babbd6e582eadae0a
lifan <75855385@qq.com>
2019-02-21 20:45:06 +0800

    role开发完毕

--- src/components/Role.vue
+++ src/components/Role.vue
@@ -8,15 +8,85 @@
     <el-card class="box-card">
       <el-button type="primary">添加角色</el-button>
       <el-table :data="roleInfo" border style="width: 100%">
+        <el-table-column type="expand" width="70">
+          <template slot-scope="info">
+            <!-- {{info.row}} -->
+            <!-- 一级权限遍历 -->
+            <el-row
+              v-for="(item,i) in info.row.children"
+              :key="item.id"
+              :style="{'border-bottom':'1px solid #eee','border-top':i===0?'1px solid #eee':''}"
+            >
+              <el-col :span="5">
+                <el-tag closable @close="closeTag(info.row.id,item.id)">{{item.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <el-col :span="19">
+                <!-- 二级权限遍历 -->
+                <el-row
+                  v-for="(item2,k) in item.children"
+                  :key="item2.id"
+                  :style="{'border-top':'1px solid #eee','border-top':k!==0?'1px solid #eee':''}"
+                >
+                  <el-col :span="6">
+                    <el-tag
+                      type="success"
+                      closable
+                      @close="closeTag(info.row.id,item2.id)"
+                    >{{item2.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <!-- 三级权限遍历 -->
+                    <el-tag
+                      type="warning"
+                      v-for="item3 in item2.children"
+                      :key="item3.id"
+                      closable
+                      @close="closeTag(info.row.id,item3.id)"
+                    >{{item3.authName}}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
         <el-table-column type="index" label="序号" width="70"></el-table-column>
         <el-table-column prop="roleName" label="名称"></el-table-column>
         <el-table-column prop="roleDesc" label="角色描述" width="200"></el-table-column>
         <el-table-column label="操作" width="350">
-          <el-button type="primary" icon="el-icon-edit" size="small">编辑</el-button>
-          <el-button type="danger" icon="el-icon-delete" size="small">删除</el-button>
-          <el-button type="warning" icon="el-icon-setting" size="small">分配权限</el-button>
+          <template slot-scope="info">
+            <el-button type="primary" icon="el-icon-edit" size="small">编辑</el-button>
+            <el-button type="danger" icon="el-icon-delete" size="small">删除</el-button>
+            <el-button
+              type="warning"
+              icon="el-icon-setting"
+              size="small"
+              @click="showDistributeDialogVisible(info.row)"
+            >分配权限</el-button>
+          </template>
         </el-table-column>
       </el-table>
+      <el-dialog title="角色分配" :visible.sync="distributeDialogVisible" width="50%">
+        <el-form :model="distributeForm" label-width="130px">
+          <el-form-item label="角色名称：">{{distributeForm.roleName}}</el-form-item>
+          <el-form-item label="供分配权限：">
+            <el-tree
+              :data="rightsInfo"
+              :props="rightsInfoProps"
+              node-key="id"
+              show-checkbox
+              :default-checked-keys="defaultCheckedKeys"
+              default-expand-all
+              ref="rightsTree"
+            ></el-tree>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="distributeDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="distributeRights">确 定</el-button>
+        </span>
+      </el-dialog>
     </el-card>
   </div>
 </template>
@@ -28,7 +98,18 @@ export default {
   },
   data() {
     return {
-      roleInfo: []
+      roleInfo: [],
+      distributeForm: {
+        roleName: '',
+        id: 0
+      },
+      distributeDialogVisible: false,
+      rightsInfo: [],
+      rightsInfoProps: {
+        label: 'authName',
+        children: 'children'
+      },
+      defaultCheckedKeys: []
     }
   },
   methods: {
@@ -39,6 +120,63 @@ export default {
       }
       this.roleInfo = res.data
       // console.log(this.roleInfo)
+    },
+    closeTag(roleId, rightsId) {
+      this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(async () => {
+          const { data: res } = await this.$http.delete(
+            `roles/${roleId}/rights/${rightsId}`
+          )
+          if (res.meta.status !== 200) {
+            return this.$message.error(res.meta.msg)
+          }
+          this.$message.success(res.meta.msg)
+          this.getRoleInfo()
+        })
+        .catch(() => {})
+    },
+    async showDistributeDialogVisible(role) {
+      this.distributeDialogVisible = true
+      this.distributeForm = role
+      const { data: res } = await this.$http.get('rights/tree')
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.rightsInfo = res.data
+      var idArr = []
+      this.getHavaRights(role, idArr)
+      this.defaultCheckedKeys = idArr
+    },
+    getHavaRights(roid, idArr) {
+      if (!roid.children) {
+        return idArr.push(roid.id)
+      }
+      roid.children.forEach(item => {
+        return this.getHavaRights(item, idArr)
+      })
+    },
+    async distributeRights() {
+      var ids1 = this.$refs.rightsTree.getCheckedKeys()
+      var ids2 = this.$refs.rightsTree.getHalfCheckedKeys()
+      var allIds = [...ids1, ...ids2].join(',')
+      console.log(allIds)
+      if (!allIds) {
+        return this.$message.error('请选择权限')
+      }
+      const { data: res } = await this.$http.post(
+        `roles/${this.distributeForm.id}/rights`,
+        { rids: allIds }
+      )
+      if (res.meta.status !== 200) {
+        return this.$message.error(res.meta.msg)
+      }
+      this.distributeDialogVisible = false
+      this.$message.success(res.meta.msg)
+      this.getRoleInfo()
     }
   }
 }
